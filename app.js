6a4e35147bf135f5a150bb66591d6e6be9ef7898
moemen gaballa <moemengaballa@gmail.com>
2022-11-01 09:48:35 +0200

    add view port for header html

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ app.get('/not-admin', (req, res, next) => {
    })
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
    console.log('Server listen on port 3000');
 });
